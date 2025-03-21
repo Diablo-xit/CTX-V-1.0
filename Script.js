ac26e209840b8b85506b3604ba2a30a7cdf2d224
Diablo-xit <narutowahab87@gmail.com>
2025-03-21 18:01:41 +0100

    Create Script.js

---
+++ Script.js
@@ -0,0 +1,15 @@
+const date = new Date();
+document.getElementById('year').innerHTML=`${date.getFullYear()}`;
+
+  ScrollReveal().reveal('.content', { delay: 400, distance: '90px', origin: 'bottom' });
+  
+  document.getElementById('joint').addEventListener('click',function(){
+    window.location.href='https://t.me/+5U3w4sXLNsFlMGVk';
+  });
+  document.getElementById('feed').addEventListener('click',()=>{
+    window.location.href="https://wa.me/+234 07071776576"
+  })
+  AOS.init();
+  function send(){
+    window.location.href="https://m.me/j/AbZvDSRZoLwIyHLh/";
+  }
